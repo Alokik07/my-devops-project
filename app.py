f80b471a01d2d669e33034fdc49143ef7595711f
root <root@Alokikjain.localdomain>
2026-03-29 14:25:07 +0530

    final test

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "hello amisha and alokik devops"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)pollingh test
-polling test
+    app.run(host="0.0.0.0", port=5000)final devops working
