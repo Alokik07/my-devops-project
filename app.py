669cfd0a394667462560f5a121bfd507aeab00b5
root <root@Alokikjain.localdomain>
2026-03-29 13:49:02 +0530

    poll test

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ def home():
     return "hello amisha and alokik devops"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)pollingh test
+polling test
